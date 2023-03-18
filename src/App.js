891a44da3cbb773b0976d3b43f70622dfb57e514
Piers Ebdon <37227905+piers12@users.noreply.github.com>
2023-03-18 16:59:17 +0000

    configure UI of header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className='container'>
-      Piers
+    <div>
+      <NavBar />
     </div>
   );
 }
