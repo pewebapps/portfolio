35d887a36a0c00128cdc390c1df1012b28b2e384
Piers Ebdon <37227905+piers12@users.noreply.github.com>
2023-03-19 08:19:21 +0000

    configure header title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./pages/Home"
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <div>
         <NavBar />
         <Routes>
-            <Route path="/" element={<p>Home</p>} />
+            <Route path="/" element={<Home/>} />
             <Route path="/projects" element={<p>Projects</p>} />
             <Route path="/contact" element={<p>Contact</p>} />
         </Routes>
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "../components/Title";
+
+function Home() {
+    return (
+        <div className="container-fluid" style={{ backgroundColor: "#5386E4" }}>
+            <Title title="Piers Ebdon"/>
+        </div>
+    )
+}
+
+export default Home
